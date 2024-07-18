1e72b8865cf13503837de46c3ce3b5e2d3b86c76
vybukhivka <99396589+vybukhivka@users.noreply.github.com>
2024-07-18 23:34:13 +0300

    hoisting and TDZ

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,6 +1,6 @@
 'use strict'
 
-function calcAge(birthYear) {
+/* function calcAge(birthYear) {
 	const age = 2037 - birthYear;
 
 	function printAge() {
@@ -34,4 +34,48 @@ function calcAge(birthYear) {
 const firstName = 'Bob';
 calcAge(999);
 // console.log(age);
-// printAge();
+// printAge(); */
+
+ console.log(me)
+ // console.log(job)
+ // console.log(year)
+
+var me = 'bob'
+// let job = 'teacher'
+// const year = 999
+
+// Functions
+console.log(addDecl(2,3))
+// console.log(addExpr(2,3))
+// console.log(addArrow(2,3))
+
+function addDecl(a, b) {
+	return a + b 
+}
+
+const addExpr = function (a,b) {
+	return a + b 
+}
+
+const addArrow = (a, b) => a + b;
+
+// Example
+if (!numProducts) {
+	deleteShoppingCard()
+}
+
+var numProducts = 10;
+
+function deleteShoppingCard() {
+	console.log('All products deleted!') 
+}
+
+var x = 1
+let y = 2
+const z = 3
+
+console.log(x === window.x)
+console.log(y === window.y)
+console.log(z === window.z)
+
+
