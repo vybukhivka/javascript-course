04d8e8ace15378678a857198a839c5913b1b9877
vybukhivka <99396589+vybukhivka@users.noreply.github.com>
2024-07-18 23:02:58 +0300

    scopes

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,37 @@
-'use strict';
+'use strict'
+
+function calcAge(birthYear) {
+	const age = 2037 - birthYear;
+
+	function printAge() {
+		let output = `${firstName} are ${age}, born in ${birthYear}`	;
+		console.log(output);
+
+		if (birthYear >= 996 && birthYear <= 1996) {
+			var millenial = true
+			// Creating new variable with same name as outer scope's variable
+			const firstName = 'Steve'
+			const str = `Oh, and you're a millenial, ${firstName}`
+			console.log(str)
+
+			function add(a, b) {
+				return a + b;
+			}
+			// console.log(add(1, 2))
+			
+			// Reassing outer scope's variable
+			output = 'NEW OUTPUT';
+		}
+		// console.log(millenial)
+		// console.log(str) 
+		console.log(output)
+	}
+	printAge();
+
+	return age;
+}
+
+const firstName = 'Bob';
+calcAge(999);
+// console.log(age);
+// printAge();
