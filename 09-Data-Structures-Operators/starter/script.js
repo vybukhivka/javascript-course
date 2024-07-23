0279b4051578abd3fc93f41244346fdf612d11a4
vybukhivka <99396589+vybukhivka@users.noreply.github.com>
2024-07-23 18:24:14 +0300

    destructuring, spread operator, rest, short circuiting, nullish operators

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -26,4 +26,232 @@ const restaurant = {
       close: 24,
     },
   },
+  order: function(starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]]
+  },
+  orderDelivery: function({ starterIndex = 1, mainIndex = 1, time = '20:00', address = 'no address' }) {
+    console.log(`Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`);
+  },
+
+  orderPasta: function(ing1, ing2, ing3) {
+    console.log(`Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}`)
+  },
+
+  orderPizza: function(mainIngredient, ...otherIngredients) {
+    console.log(mainIngredient)
+    console.log(otherIngredients)
+  }
 };
+
+// Logical assignment operators
+
+/* const rest1= {
+  name: 'Bob',
+  // numGuests: 20,
+  numGuests: 0,
+}
+
+const rest2= {
+  name: 'la Bib',
+  owner: 'Mr.Bib'
+}
+
+// rest1.numGuests = rest1.numGuests || 10;
+// rest2.numGuests = rest2.numGuests || 10;
+
+// Logical OR assignment operator
+rest1.numGuests ||= 10;
+rest2.numGuests ||= 10;
+
+// Nullish assignment operator
+rest1.numGuests ??= 10;
+rest2.numGuests ??= 10;
+
+// AND assignment
+// rest1.owner = rest1.owner && 'bibi'
+
+// Logical AND assignment operator
+rest2.owner &&= 'lol';
+
+console.log(rest1)
+console.log(rest2) */
+
+// Nullish Coalescing Operator (null or undefined === false)
+
+/* // other values === true (0 === true) 
+
+restaurant.numGuests = 0
+const guests2 = restaurant.numGuests || 10;
+console.log(guests2)
+
+const guests3 = restaurant.numGuests ?? 10;
+console.log(guests3) */
+
+// Short-circuiting
+
+/* // || returns first truethy value
+console.log(3 || 'Bob') // 3
+console.log('' || 'Bob') // Bob
+console.log(true || 0) // true
+console.log(undefined || null) // null (undefinde === false)
+console.log(undefined || 0 || '' || 'Hello' || 23 || null) // Hello
+
+restaurant.numGuests = 23
+
+// Dum circuiting
+const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
+
+// Use of short circuitings
+const guests2 = restaurant.numGuests || 10;
+console.log(guests1, guests2)
+
+console.log('----AND----')
+
+// && returns first falsy value
+console.log(3 && 'Bob') // Bob
+console.log('' && 'Bob') // ''
+console.log(true && 0) // 0
+console.log(undefined && null) // undefined
+console.log(undefined && 0 && '' && 'Hello' && 23 && null) // undefined
+
+// example
+if (restaurant.orderPizza) {
+  restaurant.orderPizza('mush', 'spich')
+}
+
+restaurant.orderPizza && restaurant.orderPizza('mush', 'spich') */
+
+// Rest pattern
+/*
+// 1. Destructuring
+
+const arr = [1,2, ...[3, 4]]; // Spread on the right side
+
+const [a, b, ...others] = [1, 2, 3, 4, 5] // Rest on the left side
+console.log(a, b, others)
+
+const [pizza, , risotto, ...otherFood] = [...restaurant.mainMenu, ...restaurant.starterMenu]
+console.log(pizza, risotto, ...otherFood)
+
+// Objects
+const { sat, ...weekdays } = restaurant.openingHours
+console.log(weekdays)
+
+// 2. Functions
+const add = function(...numbers) {
+  let sum = 0;
+  for(let i = 0; i < numbers.length; i++) sum += numbers[i]
+  console.log(sum)
+  return sum;
+}
+add(2, 3)
+add(5, 3, 7, 2)
+add(3, 1, 8, 9, 1, 5, 4)
+
+const x = [23, 5, 7]
+add(...x);
+
+const ings = ['mushrooms', 'onion', 'olives', 'spinach']
+restaurant.orderPizza(...ings) */
+
+// Spread operator
+
+/* const arr = [7, 8, 9];
+const newArr = [1, 2, ...arr]
+
+// console.log(newArr) // logs an array
+// console.log(...newArr) // logs an array values : 1 2 7 8 9 
+
+const newMenu = [...restaurant.mainMenu, 'Gnocci']
+
+// Copy array
+const mainMenuCopy = [...restaurant.mainMenu]
+
+// Join arrays
+const bothMenus = [...restaurant.mainMenu, ...restaurant.starterMenu]
+
+// Iterables: arrays, strings, maps, stes. Not objects
+const str = 'Bob';
+const letters = [...str, ' ', 'S. ']
+console.log(letters)
+console.log(...str)
+// console.log(`${...str}`)
+
+// const ingredients = [
+//   prompt('Let\'s make pasta! Ingredient 1?'),
+//   prompt('Let\'s make pasta! Ingredient 2?'),
+//   prompt('Let\'s make pasta! Ingredient 3?')
+// ]
+// restaurant.orderPasta(...ingredients)
+
+// Objects
+const newRestaurant = {foundedIn: 1999, ...restaurant, founder: 'Bob'};
+console.log(newRestaurant)
+
+const restaurantCopy = {...restaurant};
+restaurantCopy.name = ['lol']
+console.log(restaurantCopy)
+console.log(restaurant) */
+
+/* restaurant.orderDelivery({
+  starterIndex: 2,
+})
+
+const { name, openingHours, categories } = restaurant;
+console.log(name, openingHours, categories);
+
+const {
+  name: restaurantName,
+  openingHours: hours,
+  categories: tags
+} = restaurant;
+console.log(restaurantName, hours, tags)
+
+// Default values
+const { menu = [], starterMenu: starters = [] } = restaurant;
+console.log(menu, starters);
+
+// Mutating variables
+let a = 111 // initial variable
+let b = 999 // initial variable
+const obj = {a: 23, b: 7, c: 14};
+
+({a, b} = obj);
+console.log(a, b)
+
+// Nested objects
+const { fri: {open: o, close: c} } = openingHours;
+console.log(o, c) */
+
+// Destructuring basics
+
+/* const arr = [2, 3, 4];
+const a = arr[0]
+const b = arr[1]
+const c = arr[2]
+
+// Destructuring assigment
+const [x, y, z] = arr;
+
+console.log(a, b, c)
+console.log(x, y, z)
+
+let [main, , secondary] = restaurant.categories;
+
+// Switching variables
+[main, secondary] = [secondary, main];
+console.log(main, secondary)
+
+const [starter, mainCourse] = restaurant.order(2, 0);
+console.log(starter, mainCourse);
+
+// Nested destructuring
+const nested = [2, 4, [5 , 6]];
+// const [i, ,j] = nested;
+// console.log(i, j)
+const [i, , [j , k]] = nested
+console.log(i, j, k)
+
+// Default values
+const [p=1, q=1, r=1] = [8, 9];
+console.log(p, q, r); */
